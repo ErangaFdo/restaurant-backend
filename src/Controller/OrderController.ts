ddd80804e5e69b44198286323d60152da41a8f6d
ErangaFernando404 <erangafdo1994@gmail.com>
2025-12-11 21:47:13 +0530

    create getall order

--- src/Controller/OrderController.ts
+++ src/Controller/OrderController.ts
@@ -60,3 +60,28 @@ export const createOrder = async (req: Request, res: Response) => {
     }
   });
 };
+
+export const getAllOrder = async(req: Request , res:Response)=>{
+    try {
+        const page = parseInt(req.query.page as string) || 1;
+        const limit = parseInt(req.query.limit as string) || 10;
+        const skip = (page - 1) * limit;
+
+        const user = await Order.find()
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit);
+
+        const total = await Order.countDocuments();
+        return res.status(200).json({
+            message: 'Order Details get Successful',
+            data: user,
+            totalPages: Math.ceil(total / limit),
+            totalCount: total,
+            page,
+        });
+
+    } catch (error: any) {
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
+}
--- src/Routes/OrderRoutes.ts
+++ src/Routes/OrderRoutes.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import { authenticate } from "../Middleware/Auth";
 import { requireRole } from "../Middleware/role";
 import { Role } from "../Model/UserModel";
-import { createOrder } from "../Controller/OrderController";
+import { createOrder, getAllOrder } from "../Controller/OrderController";
 
 const router = Router()
 router.post("/create",  authenticate , requireRole([Role.USER]) , createOrder )
+router.get("/getall",  getAllOrder )
 
 export default router
