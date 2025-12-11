35bdcd9d5f77de428d2b605e987c3cd77f94f6ba
ErangaFernando404 <erangafdo1994@gmail.com>
2025-12-11 21:20:28 +0530

    getall feedback

--- src/Routes/FeedBackRoutes.ts
+++ src/Routes/FeedBackRoutes.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import { authenticate } from "../Middleware/Auth";
 import { requireRole } from "../Middleware/role";
 import { Role } from "../Model/UserModel";
-import { creaetFeedback } from "../Controller/FeedBackController";
+import { creaetFeedback, getAllFeedback } from "../Controller/FeedBackController";
 
 const router = Router()
 router.post("/create",  authenticate , requireRole([Role.USER]) , creaetFeedback)
+router.post("/getall",    getAllFeedback )
 
 export default router
