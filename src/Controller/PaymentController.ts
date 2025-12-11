12707d80279a7e8b5eb1fcddbe2320669d7c36df
ErangaFernando404 <erangafdo1994@gmail.com>
2025-12-11 23:05:25 +0530

    create getall payment

--- src/Controller/PaymentController.ts
+++ src/Controller/PaymentController.ts
@@ -51,3 +51,28 @@ export const createPayment = async (req: Request, res: Response) => {
     }
   });
 };
+
+export const getAllPayment = async(req: Request , res:Response)=>{
+    try {
+        const page = parseInt(req.query.page as string) || 1;
+        const limit = parseInt(req.query.limit as string) || 10;
+        const skip = (page - 1) * limit;
+
+        const user = await Payemnt.find()
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit);
+
+        const total = await Payemnt.countDocuments();
+        return res.status(200).json({
+            message: 'Payment Details get Successful',
+            data: user,
+            totalPages: Math.ceil(total / limit),
+            totalCount: total,
+            page,
+        });
+
+    } catch (error: any) {
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
+}
--- src/Routes/PaymentRoutes.ts
+++ src/Routes/PaymentRoutes.ts
@@ -2,10 +2,10 @@ import { Router } from "express";
 import { authenticate } from "../Middleware/Auth";
 import { requireRole } from "../Middleware/role";
 import { Role } from "../Model/UserModel";
-import { createPayment } from "../Controller/PaymentController";
+import { createPayment, getAllPayment } from "../Controller/PaymentController";
 
 const router = Router()
 router.post("/create",  authenticate , requireRole([Role.USER]) , createPayment )
-
+router.get("/getall",  getAllPayment )
 
 export default router
