b86bd570c2bed60f39108522095afae3d06a3c0e
ErangaFernando404 <erangafdo1994@gmail.com>
2025-12-11 10:48:50 +0530

    create register

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import authRouter from "./Routes/AuthRoutes"
+dotenv.config()
+const SERVER_PORT = process.env.SERVER_PORT
+const MONGO_URI = process.env.MONGO_URI as string
+
+
+const app = express()
+
+
+
+app.use(express.json())
+app.use("/api/v1/auth" , authRouter)
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("DB connected Successful")
+  })
+  .catch((err) => {
+    console.error(`DB connection fail: ${err}`)
+    process.exit(1)
+  })
+
+
+app.listen(SERVER_PORT,()=>{
+    console.log("Server is Running 5000")
+})
