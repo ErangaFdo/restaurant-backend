51f4b8ca0d3c3808eb58cf0cc57a060384c2916d
ErangaFernando404 <erangafdo1994@gmail.com>
2025-12-11 22:33:13 +0530

    create getall delivery

--- src/Controller/DeliveryController.ts
+++ src/Controller/DeliveryController.ts
@@ -51,3 +51,29 @@ export const createDelivery = async (req: Request, res: Response) => {
     }
   });
 };
+
+
+export const getAllDelivery = async(req: Request , res:Response)=>{
+    try {
+        const page = parseInt(req.query.page as string) || 1;
+        const limit = parseInt(req.query.limit as string) || 10;
+        const skip = (page - 1) * limit;
+
+        const user = await Delivery.find()
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit);
+
+        const total = await Delivery.countDocuments();
+        return res.status(200).json({
+            message: 'Delivery Details get Successful',
+            data: user,
+            totalPages: Math.ceil(total / limit),
+            totalCount: total,
+            page,
+        });
+
+    } catch (error: any) {
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
+}
--- src/Routes/DeliveryRoutes.ts
+++ src/Routes/DeliveryRoutes.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { authenticate } from "../Middleware/Auth";
 import { requireRole } from "../Middleware/role";
 import { Role } from "../Model/UserModel";
-import { createDelivery  } from "../Controller/deliveryController";
+import { createDelivery, getAllDelivery  } from "../Controller/deliveryController";
 
 const router = Router()
 router.post("/create",  authenticate , requireRole([Role.USER]) , createDelivery )
+router.get("/getall",    getAllDelivery )
 
 
 export default router
