cbf71e7e74f1e1fd42fc833ca6ac1ea2de5babd6
ErangaFernando404 <erangafdo1994@gmail.com>
2026-01-06 08:23:14 +0530

    update MenuRoutes

--- src/Routes/MenuRoutes.ts
+++ src/Routes/MenuRoutes.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import { authenticate } from "../Middleware/Auth";
 import { requireRole } from "../Middleware/role";
 import { Role } from "../Model/UserModel";
-import { createFood } from "../Controller/MenuController";
+import { createFood, deleteFood, getAll, searchFood, updateFood } from "../Controller/MenuController";
 import { upload } from "../Middleware/upload";
 
 const router = Router()
 router.post("/create",  authenticate , requireRole([Role.ADMIN]) , upload.single("image") , createFood)
-
+router.get("/all" , upload.single("image") , getAll)
+router.put("/updateFood/:id", authenticate , requireRole([Role.ADMIN]) , upload.single("image"), updateFood)
+router.delete("/deleteFood/:id", authenticate , requireRole([Role.ADMIN]), deleteFood)
+router.get("/search", searchFood);
 
 
 export default router
